2c94e83569006cf4a0d3f39d46c4387a033f4e06
NguyenDuyHieu987 <vaicut6941@gmail.com>
2022-12-08 13:08:09 +0700

    add

--- src/app/controllers/AuthController.js
+++ src/app/controllers/AuthController.js
@@ -2,11 +2,27 @@ const db = require('../../config/db');
 
 class UserController {
   login(req, res, next) {
-    const sql = `SELECT fullname, role FROM users WHERE username='${req.body.username}' AND password='${req.body.password}'`;
-    db.query(sql, (err, response) => {
-      if (err) throw next(err);
-      res.json(response[0]);
-    });
+    try {
+      const sql = `SELECT fullname, role, usertoken FROM users WHERE username = '${req.body.username}' AND password = '${req.body.password}'`;
+      db.query(sql, (err, response) => {
+        if (err) throw next(err);
+        res.json(response[0]);
+      });
+    } catch (error) {
+      res.status(400).json({ message: 'Error' });
+    }
+  }
+  keepLogin(req, res, next) {
+    try {
+      const sql = `SELECT fullname, role FROM users WHERE usertoken = ${req.body.usertoken}`;
+
+      db.query(sql, (err, response) => {
+        if (err) throw next(err);
+        res.json(response[0]);
+      });
+    } catch (error) {
+      res.status(400).json({ message: 'Error' });
+    }
   }
 }
 
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,5 +5,6 @@ const authController = require('../app/controllers/AuthController');
 
 // router.get('/:slug', tvController.detail);
 router.post('/login', authController.login);
+router.post('/keeplogin', authController.keepLogin);
 
 module.exports = router;
