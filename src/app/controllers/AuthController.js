4fb805d34f45c46cf094db644a4488bb247ed2ab
Dom Dom <vaicut6041@gmail.com>
2023-01-21 16:10:39 +0700

    add

--- src/app/controllers/AuthController.js
+++ src/app/controllers/AuthController.js
@@ -27,7 +27,7 @@ class UserController {
       const sql = `SELECT fullname, role, usertoken FROM users WHERE username = '${req.body.username}' AND password = '${req.body.password}'`;
       db.query(sql, (err, response) => {
         if (err) throw next(err);
-        else res.json(response[0]);
+        res.json(response[0]);
       });
     } catch (error) {
       res.status(400).json({ message: 'Error' });
